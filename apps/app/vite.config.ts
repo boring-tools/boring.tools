8ba086501a489462d65795f014a6aa0ce149d8a6
Lars Hampe <hello@hashdot.co>
2024-09-30 19:35:38 +0200

    feat(app): add docker build

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../../build/app',
+    emptyOutDir: true,
+  },
   plugins: [TanStackRouterVite(), react()],
 })
