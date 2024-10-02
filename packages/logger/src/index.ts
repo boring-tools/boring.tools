2194bcc9eea2e6941d58e74e741d51721c368fae
Lars Hampe <hello@hashdot.co>
2024-10-02 15:44:20 +0200

    feat(logger): use logtail only in dev/prod

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -2,14 +2,13 @@ import { Logtail } from '@logtail/node'
 import { LogtailTransport } from '@logtail/winston'
 import winston from 'winston'
 
-// Create a Logtail client
-const logtail = new Logtail(process.env.BETTERSTACK_LOG_TOKEN as string)
-
 // Create a Winston logger - passing in the Logtail transport
 export const logger = winston.createLogger({
   transports: [],
 })
 
 if (import.meta.env.NODE_ENV !== 'test') {
+  // Create a Logtail client
+  const logtail = new Logtail(process.env.BETTERSTACK_LOG_TOKEN as string)
   logger.add(new LogtailTransport(logtail))
 }
