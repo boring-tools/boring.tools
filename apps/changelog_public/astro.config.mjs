f527594cee5c7cc48f693dfe5ab6181a15cf2ac1
Lars Hampe <hello@hashdot.co>
2024-10-24 21:32:20 +0200

    chore: fix page app build errors

--- apps/changelog_public/astro.config.mjs
+++ apps/changelog_public/astro.config.mjs
@@ -5,8 +5,15 @@ import react from '@astrojs/react'
 
 import tailwind from '@astrojs/tailwind'
 
+import node from '@astrojs/node'
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  outDir: '../../build/page',
   integrations: [react(), tailwind({ nesting: true })],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
